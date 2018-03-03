a11e995fc4b19d50c63c26c25cd7f40124ce62dc
Xiyue Hu <xiyueh.hxy@gmail.com>
2018-03-03 22:54:30 +1000

    tidy up format and add relevant content

---
+++ js/index.js
@@ -0,0 +1,66 @@
+var trafficChartCanvas = document.getElementById("trafficChart");
+var usageChartCanvas = document.getElementById("usageChart");
+var trafficChartCanvas2 = document.getElementById("trafficChart2");
+
+var trafficChart = new Chart(trafficChartCanvas, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [40, 60],
+            backgroundColor: [
+                // 'rgb(255, 99, 132)',
+                'rgb(255, 205, 86)',
+                'rgb(54, 162, 235)'
+            ]
+        }],
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            // 'Red',
+            'Checked In',
+            'Not Checked In'
+        ]
+    },
+    // options: options
+});
+
+var usageChart = new Chart(usageChartCanvas, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [70, 30],
+            backgroundColor: [
+                // 'rgb(255, 99, 132)',
+                'rgb(255, 205, 86)',
+                'rgb(54, 162, 235)'
+            ]
+        }],
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            // 'Red',
+            'Being Used',
+            'Not Being Used'
+        ]
+    },
+    // options: options
+});
+
+var trafficChart2 = new Chart(trafficChartCanvas2, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [40, 60],
+            backgroundColor: [
+                // 'rgb(255, 99, 132)',
+                'rgb(255, 205, 86)',
+                'rgb(54, 162, 235)'
+            ]
+        }],
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            // 'Red',
+            'Checked In',
+            'Not Checked In'
+        ]
+    },
+    // options: options
+});
